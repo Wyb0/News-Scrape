d58e30d77f6e3a682c03648cd03ba2f50cd2991c
Andrew Wybo <andrewwybo@gmail.com>
2017-10-17 23:00:19 -0400

    working server, added require for handlebars

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require("express")
 var bodyParser = require("body-parser")
+var exphbrs = require('express-handlebars')
 var path = require('path')
 
 var app = express();
+
+var PORT = process.env.PORT || 8000
+
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+    console.log("Listening on Port: " + PORT)
+})
